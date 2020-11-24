9a442c20aaabd7d48a10f15e5e276084884a35b0
shivanshk <shivansh.kuchchal@dltlabs.io>
2020-11-24 20:03:34 +0530

    Validate method complete for Value.

--- value.go
+++ value.go
@@ -5,5 +5,13 @@ type Value struct {
 }
 
 func (v *Value) Validate(arg interface{}, customErr error) error {
+	for _, rule := range v.Rules {
+		if rule == nil {
+			continue
+		}
+		if err := rule(arg); err != nil {
+			return orErr(customErr, err)
+		}
+	}
 	return nil
 }
