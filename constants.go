d969236cb46e3471435cb130f8e01ba3665d0724
shivanshk <shivansh.kuchchal@dltlabs.io>
2020-11-25 19:23:02 +0530

    UUIDv4 Regex corrected.

--- constants.go
+++ constants.go
@@ -1,5 +1,5 @@
 package valkyrie
 
 const (
-	uuidRegex = "^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$"
+	uuidRegex = "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$"
 )
