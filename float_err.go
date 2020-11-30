6a8941df9aaff2cd0af64de17527f7d1ff0e89ef
shivanshk <shivansh.kuchchal@dltlabs.io>
2020-11-30 13:58:49 +0530

    Improved int-float error messages.

--- float_err.go
+++ float_err.go
@@ -4,8 +4,8 @@ import "fmt"
 
 var (
 	errFloat64  = func(dt string) error { return fmt.Errorf("value should follow: type %s && convertible to float64", dt) }
-	errFloatGTE = func(value float64) error { return fmt.Errorf("value should follow: >= %f", value) }
-	errFloatLTE = func(value float64) error { return fmt.Errorf("value should follow: <= %f", value) }
-	errFloatGT  = func(value float64) error { return fmt.Errorf("value should follow: > %f", value) }
-	errFloatLT  = func(value float64) error { return fmt.Errorf("value should follow: < %f", value) }
+	errFloatGTE = func(value float64) error { return fmt.Errorf("value should follow: type float64 && >= %f", value) }
+	errFloatLTE = func(value float64) error { return fmt.Errorf("value should follow: type float64 && <= %f", value) }
+	errFloatGT  = func(value float64) error { return fmt.Errorf("value should follow: type float64 && > %f", value) }
+	errFloatLT  = func(value float64) error { return fmt.Errorf("value should follow: type float64 && < %f", value) }
 )
